bb23d4da1d1f44952acaf451d62fe0e4d87cb777
daebangstn <gunho1107@naver.com>
2024-06-04 11:26:42 +0900

    dependency

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "scipy",
         "torch>1.6.0",
         "tqdm",
-        "pyglet==1.5.27",
+        "pyglet==2.0.15",
         "PyOpenGL==3.1.0",
         "PyOpenGL-accelerate",
         "body_visualizer @ git+https://github.com/nghorbani/body_visualizer.git@be9cf756f8d1daed870d4c7ad1aa5cc3478a546c",
